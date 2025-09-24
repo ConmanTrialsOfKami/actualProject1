c8424b4b5a03dd4082616e205dcf3cd0ac8a6011
ConmanTrialsOfKami <111887700+ConmanTrialsOfKami@users.noreply.github.com>
2025-09-23 20:03:49 -0400

    Node beginning

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
 
 using namespace std;
+// Node of BST/AVL
+struct Node {
+    string name; //student name
+    int id; //UF id
+    int height; //height of AVL
+    Node* left;
+    Node* right;
 
+    Node(string& n, int i) {
+        name = n;
+        id = i;
+        height = 1;
+        left = nullptr;
+        right = nullptr;
+
+    }
+};
 int main(){
-	cout << "Hello AVL!\n";
-	return 0;
+
 }

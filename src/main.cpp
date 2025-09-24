a38f8261bd202baa277795531fa19f477733a6fe
ConmanTrialsOfKami <111887700+ConmanTrialsOfKami@users.noreply.github.com>
2025-09-23 20:18:19 -0400

    height and heightchanger

--- src/main.cpp
+++ src/main.cpp
@@ -18,6 +18,31 @@ struct Node {
 
     }
 };
+
+int heightShower(Node* n) {
+    if (n == nullptr) {
+        return 0; // if tree empty height is 0
+    }
+    else {
+        return n->height; //Return height of tree
+    }
+}
+
+void heightUpdater(Node* n) {
+    if (n != nullptr) {
+        int leftHeight = heightShower(n->left); //gets height from left child
+        int rightHeight = heightShower(n->right); // gets height from right child
+
+        if (leftHeight > rightHeight) {
+            n->height = 1 + leftHeight; // height = 1 (this node) + taller child height
+        }
+        else {
+            n->height = 1 + rightHeight; // same thing but for rightHeight
+        }
+    }
+}
+
+
 int main(){
 
 }
